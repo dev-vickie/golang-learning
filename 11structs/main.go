7a15053d4d7660aaabfa01f734187ca57ee727a1
dev-vickie <vmutethia84@gmail.com>
2023-01-13 07:20:54 +0300

    Learnt about structs

---
+++ 11structs/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to structs in golang")
+
+	victor := User{"Victor max", 14, "vic@go.dev", true} //create an "object" type User
+	fmt.Println(victor)
+
+	fmt.Printf("User details are: %+v\n", victor) //%+v gives details in an ordered manner
+
+	fmt.Printf("Name is: %v ,and email is: %v", victor.Name, victor.Email)
+}
+
+type User struct { //Use uppercase for the struct's name
+	Name       string //Also uppercase for the struct's properties
+	Age        int
+	Email      string
+	Isverified bool
+}
