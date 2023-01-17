ad38906c39855d67650ff017d5a4027f607e1f10
dev-vickie <vmutethia84@gmail.com>
2023-01-17 06:28:09 +0300

    revised on making web requests

--- 19webrequests/main.go
+++ 19webrequests/main.go
@@ -11,17 +11,17 @@ const url = "https://google.com"
 func main() {
 	fmt.Println("Welcome to web requests in golang")
 
-	response, err := http.Get(url)
+	response, err := http.Get(url) //get a response from the website
 	if err != nil {
 		panic(err)
 	}
 
-	defer response.Body.Close() //caller's responsibility to close the connection
-	databytes, err := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()                     //caller's responsibility to close the connection
+	databytes, err := ioutil.ReadAll(response.Body) //read the response.Body to convert the response to databytes
 	if err != nil {
 		panic(err)
 	}
-	content := string(databytes)
+	content := string(databytes) //the databytes are then converted to string type
 
 	fmt.Println(content)
 
